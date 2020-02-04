e35c49262d3ba28d49407161ce34a4a5f0f103d5
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2020-02-04 20:13:38 +0900

    ex8_05.done

---
+++ ex8_05.py
@@ -0,0 +1,14 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if line.startswith('From '):
+        address=line.split()
+        count=count+1
+        print(address[1])
+    else : continue
+
+print("There were", count, "lines in the file with From as the first word")
