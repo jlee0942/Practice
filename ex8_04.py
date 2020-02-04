bc340ef29f59aea23e5b5fc1add0e4381c37b8ba
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2020-02-04 19:59:05 +0900

    ok

--- ex8_04.py
+++ ex8_04.py
@@ -1,9 +1,17 @@
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
+i=0
 for line in fh:
     f=line.split()
     for word in f:
         lst.append(word)
+
 lst.sort()
+while i<(len(lst)-1):
+    if lst[i]==lst[i+1]:
+        lst.remove(lst[i+1])
+    else:
+        i=i+1
+
 print(lst)
